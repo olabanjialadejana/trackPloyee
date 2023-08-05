655758386b3c95b2559f34e504ae18b8c0656618
Olabanji Aladejana <olabanjialadejana@gmail.com>
2023-08-05 11:26:37 +0100

    username and password entries added

--- login_UI.py
+++ login_UI.py
@@ -5,10 +5,11 @@ import datetime
 # ------------------------------------ SCREEN TIME FUNCTION ------------------------------------ #
 def update_time():
 	now = datetime.datetime.now()
-	# current_time = now.strftime("%H:%M:%S\n%d/%m/%Y")
-	current_time = now.strftime('%H:%M:%S\n%A, %B %d')
+	current_date = now.strftime('%A, %B %d')
+	current_time = now.strftime('%I:%M %p')
 	# make canvas2 a global variable in this function
 	canvas2.itemconfig(time_text, text=current_time)
+	canvas2.itemconfig(date_text, text=current_date)
 	# Update the time after every 1000 milliseconds (1 second)
 	window.after(1000, update_time)
 
@@ -30,10 +31,23 @@ canvas.grid(column=0, row=0)
 canvas2 = Canvas(width=900, height=450)
 canvas2.create_rectangle(210, 400, 690, 50, fill="#9DB2BF")
 canvas2.config(background="#526D82")
-# initial_time = datetime.datetime.now().strftime("%H:%M:%S\n%d/%m/%Y")
-initial_time = datetime.datetime.now().strftime('%H:%M:%S\n%A, %B %d')
-time_text = canvas2.create_text(450, 225, text=initial_time, font=("Arial", 24))
+initial_time = datetime.datetime.now().strftime('%I:%M %p')
+initial_date = datetime.datetime.now().strftime('%A, %B %d')
+date_text = canvas2.create_text(450, 200, text=initial_date, font=("Arial", 10))
+time_text = canvas2.create_text(450, 150, text=initial_time, font=("Arial", 24))
 canvas2.grid(column=0, row=1)
+
+# Username Entry
+username_entry = Entry(width=32, fg='grey')
+username_entry.focus()
+username_entry.insert(0, "Username")
+canvas2.create_window(450, 230, window=username_entry)
+
+# Password Entry
+password_entry = Entry(width=32, fg='grey')
+password_entry.focus()
+password_entry.insert(0, "Password")
+canvas2.create_window(450, 260, window=password_entry)
 
 
 
