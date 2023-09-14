ea56f646bdb8c7876b02d3ab74b1a42019c323b2
Olabanji Aladejana <olabanjialadejana@gmail.com>
2023-09-14 15:39:34 +0100

    manager dashboard completed

--- manager_dashboard_UI.py
+++ manager_dashboard_UI.py
@@ -1,65 +1,88 @@
 from tkinter import *
-from tkinter import ttk  # Import ttk for the Treeview widget
+from tkinter import ttk
 from welcome_page_UI import WelcomePageUI
+import json
+from tkinter import messagebox
 
 
 class ManagerDashboard_UI:
-	def __init__(self):
-		self.window = Tk()
-		self.window.title("Admin Dashboard")
-		self.window.geometry("900x600")
-		self.window.config(background="#27374D")
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Admin Dashboard")
+        self.window.geometry("900x600")
+        self.window.config(background="#27374D")
 
-		self.canvas = Canvas(width=900, height=100, background="#DDE6ED")
-		self.logo_img = PhotoImage(file="logo.png")
-		self.canvas.create_image(450, 50, image=self.logo_img)
-		self.canvas.grid(column=0, row=0)
+        self.canvas = Canvas(width=900, height=100, background="#DDE6ED")
+        self.logo_img = PhotoImage(file="logo.png")
+        self.canvas.create_image(450, 50, image=self.logo_img)
+        self.canvas.grid(column=0, row=0)
 
-		self.go_back_img = PhotoImage(file="samples/welcome-page-logos/icons8-go-back-48.png")
-		self.go_back_button = Button(width=48, height=48, text='go_back',
-									 image=self.go_back_img, command=self.go_back_to_welcome_page)
-		# Position the button at the upper left part of the canvas
-		self.canvas.create_window(10, 10, anchor="nw", window=self.go_back_button)
+        self.go_back_img = PhotoImage(file="samples/welcome-page-logos/icons8-go-back-48.png")
+        self.go_back_button = Button(width=48, height=48, text='go_back',
+                                     image=self.go_back_img, command=self.go_back_to_welcome_page)
+        # Position the button at the upper left part of the canvas
+        self.canvas.create_window(10, 10, anchor="nw", window=self.go_back_button)
 
-		self.canvas2 = Canvas(width=900, height=500, background="#526D82")
-		self.canvas2.create_rectangle(10, 490, 890, 10, fill="#9DB2BF")
-		self.canvas2.grid(column=0, row=1)
+        self.canvas2 = Canvas(width=900, height=500, background="#526D82")
+        self.canvas2.create_rectangle(10, 990, 890, 40, fill="#9DB2BF")
+        self.canvas2.grid(column=0, row=1)
 
-		# Create a custom style for column headers
-		bold_font = ('TkDefaultFont', 10, 'bold')  # Change the font size and style as needed
-		style = ttk.Style()
-		style.configure("Bold.Treeview.Heading", font=bold_font)
+        # Create a custom style for column headers
+        bold_font = ('TkDefaultFont', 10, 'bold')  # Change the font size and style as needed
+        style = ttk.Style()
+        style.configure("Bold.Treeview.Heading", font=bold_font)
 
-		# Create the table (treeview)
-		self.table = ttk.Treeview(columns=('staff_number', 'first_name', 'last_name', 'age', 'telephone', 'email', 'department_unit', 'position', 'pay'), show='headings', style="Bold.Treeview")
-		self.table.heading('staff_number', text='Staff Number')
-		self.table.heading('first_name', text='First Name')
-		self.table.heading('last_name', text='Last Name')
-		self.table.heading('age', text='Age')
-		self.table.heading('telephone', text='Telephone')
-		self.table.heading('email', text='Email')
-		self.table.heading('department_unit', text='Department/Unit')
-		self.table.heading('position', text='Position')
-		self.table.heading('pay', text='Pay')
+        # Create the table (treeview)
+        self.table = ttk.Treeview(columns=(
+            'staff_number', 'first_name', 'last_name', 'age', 'telephone', 'email', 'department_unit', 'position',
+            'pay'),
+            show='headings', style="Bold.Treeview")
+        self.table.heading('staff_number', text='Staff Number')
+        self.table.heading('first_name', text='First Name')
+        self.table.heading('last_name', text='Last Name')
+        self.table.heading('age', text='Age')
+        self.table.heading('telephone', text='Telephone')
+        self.table.heading('email', text='Email')
+        self.table.heading('department_unit', text='Department/Unit')
+        self.table.heading('position', text='Position')
+        self.table.heading('pay', text='Pay')
 
-		# Define the column widths to fit within the rectangle
-		self.table.column('staff_number', width=105)
-		self.table.column('first_name', width=110)
-		self.table.column('last_name', width=100)
-		self.table.column('age', width=40)
-		self.table.column('telephone', width=100)
-		self.table.column('email', width=150)
-		self.table.column('department_unit', width=120)
-		self.table.column('position', width=70)
-		self.table.column('pay', width=80)
+        # Define the column widths to fit within the rectangle
+        self.table.column('staff_number', width=105)
+        self.table.column('first_name', width=110)
+        self.table.column('last_name', width=100)
+        self.table.column('age', width=40)
+        self.table.column('telephone', width=100)
+        self.table.column('email', width=150)
+        self.table.column('department_unit', width=120)
+        self.table.column('position', width=70)
+        self.table.column('pay', width=80)
 
-		self.canvas2.create_window(12, 60, anchor="nw", window=self.table)  # Adjust the coordinates as needed
+        self.canvas2.create_window(12, 60, anchor="nw", window=self.table)  # Adjust the coordinates as needed
 
-		self.window.mainloop()
+        self.load_data_from_json()
 
-	def go_back_to_welcome_page(self):
-		self.window.destroy()
-		WelcomePageUI()
+        self.window.mainloop()
+
+    def go_back_to_welcome_page(self):
+        self.window.destroy()
+        WelcomePageUI()
+
+    def load_data_from_json(self):
+        try:
+            with open('data.json', 'r') as json_file:
+                data = json.load(json_file)
+                for staff_number, values in data.items():
+                    # Exclude 'address', 'next of kin', 'next of kin address', 'next of kin telephone'
+                    values.pop('address', None)
+                    values.pop('next of kin', None)
+                    values.pop('next of kin address', None)
+                    values.pop('next of kin telephone', None)
+                    self.table.insert("", "end", values=(
+                        staff_number, values["first name"], values["last name"], values["age"], values["telephone"],
+                        values["email"], values["department/unit"], values["position"], values["pay"]))
+        except FileNotFoundError:
+            messagebox.showerror(title="Database Error!!!", message="No user database yet!!!")
 
 
 dashboard = ManagerDashboard_UI()
