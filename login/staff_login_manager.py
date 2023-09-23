62d156b5416579ee0b556d053fe3d17ffc312ac9
Olabanji Aladejana <olabanjialadejana@gmail.com>
2023-09-23 20:57:39 +0100

    attendance record implemented halfway

--- login/staff_login_manager.py
+++ login/staff_login_manager.py
@@ -21,13 +21,34 @@ def process_login_details(input_username, input_password):
 			new_records = list(record)
 			if input_username in new_records[0]:
 				staff_password = new_records[1]['password']
-				print(f"staff password {staff_password}")
-				print(f"input password {input_password}")
 				if input_password == staff_password:
 					now = datetime.datetime.now()
 					current_time = now.strftime('%I:%M %p')
 					messagebox.showinfo(title="Login successful!!",
 										message=f"Login time: {current_time}\nWelcome to Work!!!!")
+
+					file_path = "../database/staff_attendance_record.json"
+
+					try:
+						with open(file_path, 'r') as file:
+							existing_data = json.load(file)
+					except FileNotFoundError:
+						existing_data = []
+					# Create login record
+					record = {}
+
+					staff_number = input_username
+					login_time = datetime.datetime.now()
+					record["Staff Number"] = staff_number
+					record["Login Time"] = login_time
+
+					existing_data.append(record)
+
+					print(record)
+
+					with open(file_path, 'w') as file:
+						json.dump(existing_data, file, default=str, indent=4)
+
 				else:
 					messagebox.showerror(title="Login Error!!!", message="Wrong Password!!!")
 				break
